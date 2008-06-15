a7c9f16e9bc869f57d83b5404a93733ce2a83b78
Chronotrigger.BG <Chronotrigger.BG@bfe5a510-b74f-0410-9325-bfbc73d27d4f>
2008-06-15 20:14:19 +0000

    Вече има метод за зареждане на файлове, който приема списък от имена. Работата не е готова, защото още няма бутон за следващ/предишен комикс.

--- HelloImage.py
+++ HelloImage.py
@@ -1,5 +1,6 @@
-#HelloImage - display an image file
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+#HelloImage - display an image file
 # Simon Peverett - October 2003
 # uses the PIL module: http://www.pythonware.com/products/pil/
 # developed using ActivePython 2.2: http://www.activestate.com
@@ -9,12 +10,36 @@ import Image            #PIL
 import ImageTk          #PIL
 import sys
 import getopt
+import os
+
+"""
+Filenames е или един или няколко файла, или папка
+Във всеки от случаите програмата отваря съответните файлове
+Това е клас в нов стил, наследяващ списък - има доста готови методи :)
+"""
+class Pages:        
+    def setFiles(self, filenames = []):
+        print "ASD"
+        if len(filenames) == 1 and os.path.isdir(filenames[0]):
+            """os.walk връща 3 неща:
+           път към директорията,
+            списък от директории в нея,
+            списък от файлове в нея.
+            """
+            self.dirPath, self.dirList, self.files = os.walk(filenames[0])
+            self.pages = [ os.join(self.dirPath,filename) for filename in self.files]
+        else:
+          self.pages = filenames
 
 class MainFrame(Frame):
     def __init__(self, master=None):
         Frame.__init__(self,master)
         self.grid(sticky=N+S+E+W)
+        self.files = Pages()
         self.createMainMenu()
+        #FIXME: Hardcoded!
+        self.loadImages()
+        
         self.loadAndShowImage()
         
     def createMainMenu(self):
@@ -23,7 +48,7 @@ class MainFrame(Frame):
 
     def loadAndShowImage(self):
         try:
-            self.im = Image.open("1.jpg")
+            self.im = Image.open(self.files.pages[0])
         except Exception, e:
             print >>sys.stderr, e
             sys.exit(1)
@@ -32,7 +57,14 @@ class MainFrame(Frame):
         self.canvas.grid()
         self.photo = ImageTk.PhotoImage(self.im)
         self.item = self.canvas.create_image(10,10,anchor=NW, image=self.photo)
-
+    
+    """
+    Този метод трябва да вика File Chooser-ът и да му праща списък от
+    имена на файлове/директории.
+    """
+    def loadImages(self):
+        #FIXME: Hardcoded
+        self.files.setFiles(["1.jpg"])
 
 
 ACIFT = MainFrame()
