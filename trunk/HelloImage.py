74716360e2e95ab4ce81938c9c8800110324b449
vankata.t <vankata.t@bfe5a510-b74f-0410-9325-bfbc73d27d4f>
2008-06-15 21:03:59 +0000

    Merged with modified revision 7; not complete

--- trunk/HelloImage.py
+++ trunk/HelloImage.py
@@ -6,25 +6,25 @@
 # developed using ActivePython 2.2: http://www.activestate.com
 
 from Tkinter import *
+from tkFileDialog import askopenfilename
 import Image            #PIL
 import ImageTk          #PIL
 import sys
 import getopt
 import os
 
-"""
-Filenames е или един или няколко файла, или папка
-Във всеки от случаите програмата отваря съответните файлове
-Това е клас в нов стил, наследяващ списък - има доста готови методи :)
+""" Filenames е или един или няколко файла, или папка
+    Във всеки от случаите програмата отваря съответните файлове
+    Това е клас в нов стил, наследяващ списък - има доста готови методи :)
 """
 class Pages:        
     def setFiles(self, filenames = []):
         print "ASD"
         if len(filenames) == 1 and os.path.isdir(filenames[0]):
-            """os.walk връща 3 неща:
-           път към директорията,
-            списък от директории в нея,
-            списък от файлове в нея.
+            """ os.walk връща 3 неща:
+                път към директорията,
+                списък от директории в нея,
+                списък от файлове в нея.
             """
             self.dirPath, self.dirList, self.files = os.walk(filenames[0])
             self.pages = [ os.join(self.dirPath,filename) for filename in self.files]
@@ -36,15 +36,32 @@ class MainFrame(Frame):
         Frame.__init__(self,master)
         self.grid(sticky=N+S+E+W)
         self.files = Pages()
-        self.createMainMenu()
+        self.createMenuBar()        
         #FIXME: Hardcoded!
         self.loadImages()
         
         self.loadAndShowImage()
+
+    def selecFile():
+        self.filename = askopenfilename(filetypes=[("Jpeg Files","*.jpg"),("All Files","*")])
+        return self.filename
         
-    def createMainMenu(self):
-        self.quitButton = Button(self, text="Quit!", command=self.quit)
-        self.quitButton.grid()
+    def createMenuBar(self):
+        menubar = Frame(self, relief=RAISED,borderwidth=1)
+        menubar.grid(column=0, sticky=NW)
+
+        fileButton = Menubutton(menubar, text="File")
+        fileButton.grid(column=0, sticky=NW)
+        fileButton.menu = Menu(fileButton)
+
+        fileButton.menu.add_command(label="Open")#, command=selectFile())
+        fileButton['menu'] = fileButton.menu
+        
+
+        
+        #self.quitButton = Button(self, text="Quit!", command=quit)
+        #self.quitButton.grid()
+        return
 
     def loadAndShowImage(self):
         try:
@@ -58,8 +75,7 @@ class MainFrame(Frame):
         self.photo = ImageTk.PhotoImage(self.im)
         self.item = self.canvas.create_image(10,10,anchor=NW, image=self.photo)
     
-    """
-    Този метод трябва да вика File Chooser-ът и да му праща списък от
+    """ Този метод трябва да вика File Chooser-ът и да му праща списък от
     имена на файлове/директории.
     """
     def loadImages(self):
@@ -68,5 +84,5 @@ class MainFrame(Frame):
 
 
 ACIFT = MainFrame()
-ACIFT.master.title("ACiF7 - A Comic is Fine too")
+ACIFT.master.title("ACiF7 - A Comic is Fine, too")
 ACIFT.mainloop()   
