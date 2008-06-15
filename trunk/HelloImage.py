1213d5d522d715190be8442aee964ca6c9179efe
Chronotrigger.BG <Chronotrigger.BG@bfe5a510-b74f-0410-9325-bfbc73d27d4f>
2008-06-15 18:51:15 +0000

    First actually working revision

--- trunk/HelloImage.py
+++ trunk/HelloImage.py
@@ -10,28 +10,29 @@ import ImageTk          #PIL
 import sys
 import getopt
 
-
-
 class ACIF(Frame):
-    def __init__(self, master=None):
-    	Frame.__init__(self,master)
-    	#self = TK()
-        self.pack(fill=BOTH, expand=1)
-        im = Image.open("1.jpg")
-        self.canvas = Canvas(self, height=1000, width=500)
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
-        self.canvas.grid(row=0, column=0, sticky='nesw')
-        photo = ImageTk.PhotoImage(im)
-        item = self.canvas.create_image(10,10,anchor=NW, image=photo)
+	def __init__(self, master=None):
+		#Frame.__init__(self,master)
+		self.im = Image.open("1.jpg")
+		self.canvas = Canvas(master, height=self.im.size[1]+20, width=self.im.size[0]+20)
+		self.canvas.pack(side=LEFT,fill=BOTH,expand=1)
+		self.photo = ImageTk.PhotoImage(self.im)
+		self.item = self.аcanvas.create_image(10,10,anchor=NW, image=self.photo)
+		print "ASD"
 
-execute = ACIF()
-execute.master.title("ACIF - A Comic is Fine too")
+
+tk = Tk()
+tk.title("ACIF - A Comic is Fine too")
+
+#execute.master.title("ACIF - A Comic is Fine too")
 try:
-    execute.mainloop()
-
+	print "ASDD"
+	execute = ACIF(tk)
+	tk.mainloop()	
+    
 except Exception, e:
     print >>sys.stderr, e
     print "USAGE: HelloImage <image filename>"
     sys.exit(1)
+
 
