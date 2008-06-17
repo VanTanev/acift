b5f4c0faf20a8453d614a269273a55b3b817b670
Chronotrigger.BG <Chronotrigger.BG@bfe5a510-b74f-0410-9325-bfbc73d27d4f>
2008-06-17 22:38:09 +0000

    Rejoice, mortals, for on this great day your Lord doth cometh in shining glory! This release marks another milestone passed, as we now have a functional next/previous system :) Also, the way bindings work is becoming clearer to me. If a single file is selected, then all the files in its directory are cycled. If more than one file is selected, the program cycles only through them. If the selection is da directory, the program opens all files in that directory. Okay, I haven't actually tested that last one :) By the way, a standing issue now becomes the compilation of a list of all supported file types.

--- controlFrames.py
+++ controlFrames.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from tkFileDialog import askopenfilenames as getFilenames #Лигавя се :)
+
 import os
 
 class ControlFrames:
@@ -9,11 +10,23 @@ class ControlFrames:
     Това е клас в нов стил, наследяващ списък - има доста готови методи :)
     """
     def openFiles(self):
-        filenames = [file for file in getFilenames(filetypes=[("Jpeg Files","*.jpg"),("All Files","*")])]
-        print filenames
-        if len(filenames) == 1 and os.path.isdir(filenames[0]):
-            """
-            os.path.walk връща 3 неща - предполагам е очевидно кои са :)
-            """
-            self.dirPath, self.dirList, self.files = os.walk(filenames[0])        
-        return filenames
+        #filenames = [file for file in getFilenames(filetypes=[("Jpeg Files","*.jpg"),("All Files","*")])]
+        filesToOpen = getFilenames(filetypes = [("Jpeg Files","*.jpg"),("All Files","*")], multiple = True)
+        if len(filesToOpen) == 1 and os.path.isdir(filesToOpen[0]):
+            self.dir = filesToOpen
+            #TODO: Optimize next line and make it work for all types
+            filesToOpen = [file for file in os.listdir(self.dir) if file.endswith(".jpg")]
+            firstFile = 0
+        else:
+            self.dir = os.path.split(filesToOpen[0])[0]
+            if len(filesToOpen) == 1:
+                firstFile = filesToOpen[0]
+                filesToOpen = [ os.path.join(self.dir,file) for file in os.listdir(self.dir) if file.endswith(".jpg")]
+                print firstFile
+                print filesToOpen
+                firstFile = filesToOpen.index(firstFile)
+            else:
+                firstFile = 0
+        print "     Opening files:", filesToOpen, "Starting with:", firstFile
+        return self.dir, filesToOpen, firstFile
+
