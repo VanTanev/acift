24b5ff686954248747189efe2cc2b783f0078442
Chronotrigger.BG <Chronotrigger.BG@bfe5a510-b74f-0410-9325-bfbc73d27d4f>
2008-06-25 00:28:58 +0000

    This is a big update. More things are now separated in different classes in order to have an easier work process. Not everything is done here, btw. Separated different types of input sources(files, archive) and made it invisible to the main program which one we are using (that way we can add more whenever we want). Also added a resize function and makeBigger/makeSmaller methods. Added FitToScreen. These work, though they DO need to get prettier. Need to make better filters. Basically, this is turning out nice.

--- utils.py
+++ utils.py
@@ -6,6 +6,24 @@ can look pretty.
 from menu import *
 from ImageClass import *
 
+"""
+Nice trick - keep this in mind :)
+source = self.source
+            source.one = "ASDASDASDASDASDASD"
+            print self.source.one
+"""
+
+"""
+From ImageClass
+Old code:
+    def getImage(self):
+        if self.isZip == True:
+            self.stream = StringIO(self.zip.read(self.files[self.current]))
+            return self.stream
+            return Image.open(self.stream)
+        return self.dir + self.files[self.current]
+        return Image.open(self.dir + self.files[self.current])
+"""
 
 def get_events():
     return [_ for _ in dir(wx) if _.startswith("EVT") ]
